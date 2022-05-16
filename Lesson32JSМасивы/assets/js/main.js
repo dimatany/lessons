8db969078645a20c713bd865ea0b5159c6090e10
Dima Tatiana <Cheize2020@gmail.com>
2022-05-16 12:40:20 +0300

    changes 1

--- Lesson32JSМасивы/assets/js/main.js
+++ Lesson32JSМасивы/assets/js/main.js
@@ -1,135 +1,114 @@
-"use strict"
-//Задачи из LMS
-
-//1. Мінімум
-
-// 1. Напиши всі можливі варіанти створення функцій.
-function declaration() {
-}
-
-let expression = function() {
-
-};
-
-// 2. Створи функцію, яка буде виводити кількість переданих їй аргументів.
-
-let getNum = function () {
-    alert(arguments.length);
-};
-getNum(1,3,4,5,6,1,1,1,1,1,1);//11 штук
-
-
-/* 3. Напиши функцію, яка приймає 2 числа і повертає :
-    -1, якщо перше число менше, ніж друге;
-    1 - якщо перше число більше, ніж друге;
-    0 - якщо числа рівні.
-*/
-let num = 4;
-let num1 = 4;
-function getValue(a, b) {
-    if (a < b) {
-        alert('-1');
-    } else if (a > b) {
-        alert('1');
-    } else if (a === b) {
-        alert('0');
-    } else {
-        alert('введите правильное, желательно числовое значение')
-    }
-}
-getValue(num, num1);
-
-
-// 4. Напиши функцію, яка обчислює факторіал переданого їй числа.
-function factorial(n) {
-    if (n <= 0) { //если мы попытаемся найти факториал 1,
-        return 1; //возвращаем 1 это базовый случай.
-    }
-    //в остальных случаях возвращаем произведение n на факториал предыдущего числа,
-    // таким образом мы от n дойдем до 1, перебрав каждое число.
-    return n * factorial(n - 1)
-}
-document.getElementById('task_1.4._result').innerText = factorial(4);
-
-// 5. Напиши функцію, яка приймає три окремі цифри і перетворює їх в одне число.
-// Наприклад: цифри 1, 4, 9 перетворяться в число 149.
-let number1 = 1;
-let number2 = 4;
-let number3 = 9;
-
-function createString(a,b,c) {
-    return ("" + a + b + c);
-}
-alert(createString(number1, number2, number3));
-
-
-// 6. Напиши функцію, яка приймає довжину і ширину прямокутника і обчислює його площу.
-// Якщо в функцію передали 1 параметр, то вона обчислює площу квадрата.
-
-let widthFigure = 10;
-let lengthFigure = 15;
-
-function calcSquare(a, b) {
-    if (a <= 0) {
-        a = b;
-    } else if (b <= 0)  {
-        b = a;
-    }
-    return (a * b);//вычисляем площадь фигуры по переданным параметрам
-}
-alert(calcSquare(widthFigure, lengthFigure));//как встроить второе условие когда передан только один параметр?????
-
-
-//2. Норма
-
-// 2.1. Напиши функцію, яка перевіряє, чи є передане їй число “досконалим числом”.
-// Досконале число - це число, яке дорівнює сумі всіх своїх дільників.
-
-function getAndCheck (inputId = '') {
-    let number = document.getElementById(inputId).value;
-    if (number!=='') {
-        number = parseInt(number);
-        if (!isNaN(number)) {
-            return number;
-        } else {
-            return false;
+const topPanel = {
+    show: function(text, className){
+        let panel = `<div id="top-panel" class="top-panel ${className}">${text}</div>`;
+        if(document.getElementById('top-panel') !== null){
+            document.getElementById('top-panel').remove();
         }
+        document.body.insertAdjacentHTML('afterbegin', panel);
+        this.hide();
+    },
+    hide: function(){
+        setTimeout(function(){
+            document.getElementById('top-panel').remove();
+        }, 3000);
+    },
+    error: function(text){
+        this.show(text, 'panel-error');   
+    },
+    success: function(text){
+        this.show(text, 'panel-success');   
+    },
+    info: function(text){
+        this.show(text, 'panel-info');   
+    },
+}
+
+const CART = [
+    {
+        name: 'Voter',
+        qty: 12,
+        isBuy: false,
+        price: 10,
+        total: 120
+        
+    },
+    {
+        name: 'Milk',
+        qty: 1,
+        isBuy: false,
+        price: 23.45,
+        total: 23.45
+        
+    }];
+    viewCartTable();
+function addToCart(name, qty, price){
+    if(CART.find(el => el.name===name) === undefined){
+        CART.push({
+            name: name,
+            qty: qty,
+            isBuy: false,
+            price: price,
+            total: parseFloat((qty * price).toFixed(2))
+    });
+    topPanel.success('Product successfully added');
     } else {
-        return false;
+        const prodIndex = CART.findIndex(el => el.name===name);
+        const newQty = CART[prodIndex].qty + qty;
+        //CART[prodIndex].qty;
+        CART[prodIndex].qty = newQty;
+        CART[prodIndex].total = parseFloat((newQty *CART[prodIndex].price).toFixed(2));
+        topPanel.success('Product successfully chenged');
     }
+    viewCartTable();
 }
+//addToCart('Milk', 2, 23.45);
 
-function showResult(result, whereId) {
-    document.getElementById(whereId).innerHTML = result;
-}
-
-function isNumberPerfect(n) {
-    let sum = 0;
-    for (let i = 1;  i < n; i++ ) {
-        if (n%i===0) {
-            sum += i;
-        }
+function checkAndAddToCard(){
+     let name = document.getElementById('product_name').value,
+         qty = parseInt(document.getElementById('product_qty').value),
+         price = parseFloat(document.getElementById('product_price').value);
+    let valid = true;
+     if(name === '')
+     {
+        topPanel.error('Enter product name');
+         valid = false;
+     }   
+     if (isNaN(qty)){
+        topPanel.error('Enter quantity valid value');
+         valid = false;
+     }
+     if (qty <= 0){
+        topPanel.error('Quantity must be positive');
+        valid = false;
     }
-    return sum === n;
+    if (isNaN(price)){
+        topPanel.error('Enter price valid value'); 
+        valid = false;
+    }
+    if (price <= 0){
+        topPanel.error('Price must be positive');
+       valid = false;
+   }
+   if(valid){
+       addToCart(name, qty, price);
+       
+       document.getElementById('product_name').value = '';
+       document.getElementById('product_qty').value = 1;
+       document.getElementById('product_price').value = '';
+   }
+}   
+
+function viewCartTable(){
+    let html = '';
+    CART.forEach(product => {
+        html += `
+            <tr>
+                <td>${product.name}</td>
+                <td>${product.qty}</td>
+                <td>${product.price.toFixed(2)}</td>
+                <td>${product.total.toFixed(2)}</td>
+            </tr>
+        `;
+    });
+    document.getElementById('cart-tbody').innerHTML = html;
 }
-
-function task1() {
-      let num = 0,
-          rez = '';
-       if (getAndCheck('task_2.1._number')!==false) {
-           num = getAndCheck('task_2.1._number');
-       } else {
-           return false;
-       }
-       if (isNumberPerfect(num)) {
-           rez = `число ${num} э досконалим`;
-       } else {
-           rez = `число ${num} нэ э досконалим`;
-       }
-       showResult(rez,  'task_2.1._result');
-}
-
-// 2.2.  Напиши функцію, яка приймає мінімальне і максимальне значення для діапазону,
-// і виводить тільки ті числа з діапазону, які є досконалими.
-// Використовуй написану раніше функцію, щоб дізнатися, чи є це число досконалим.
-
