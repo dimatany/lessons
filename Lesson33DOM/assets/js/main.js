7aa5345447fa4f2072a944b3ef002564b5f8f3c0
Dima Tatiana <Cheize2020@gmail.com>
2022-05-24 17:44:36 +0300

    changes 1

--- Lesson33DOM/assets/js/main.js
+++ Lesson33DOM/assets/js/main.js
@@ -1,2 +1,37 @@
 "use strict"
 
+let playList = [
+    {
+        author: "LED ZEPPELIN",
+        song:"STAIRWAY TO HEAVEN"
+    },
+    {
+        author: "QUEEN",
+        song:"BOHEMIAN RHAPSODY"
+    },
+    
+    {
+        author: "LYNYRD SKYNYRD",
+        song:"FREE BIRD"
+    },
+    {
+        author: "DEEP PURPLE",
+        song:"SMOKE ON THE WATER"
+    },
+    {
+        author: "JIMI HENDRIX",
+        song:"ALL ALONG THE WATCHTOWER"
+    },
+    {
+        author: "AC/DC",
+        song:"BACK IN BLACK"
+    },
+    {
+        author: "QUEEN",
+        song:"WE WILL ROCK YOU"
+    },
+    {
+        author: "METALLICA",
+        song:"ENTER SANDMAN"
+    }
+];
