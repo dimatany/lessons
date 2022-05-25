c6e8237482df4582d1af51fe907a44496ec1e964
Dima Tatiana <Cheize2020@gmail.com>
2022-05-25 13:18:48 +0300

    changes 1

--- Lesson33DOM/assets/js/main.js
+++ Lesson33DOM/assets/js/main.js
@@ -1,4 +1,8 @@
 "use strict"
+//Створити сторінку, що показує нумерований список пісень:
+let div = document.getElementById('list');
+let ol = document.createElement('ol');
+div.appendChild(ol);
 
 let playList = [
     {
@@ -35,3 +39,62 @@ let playList = [
         song:"ENTER SANDMAN"
     }
 ];
+
+for (let i = 0, ln = playList.length; i < ln; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = playList[i].song;
+    ol.appendChild(li);
+}
+
+
+//Створити HTML-сторінку з кнопкою "Відкрити" і модальним вікном.
+// На модальном вікні повинен бути текст і кнопка "Закрити".
+// Спочатку модальне вікно не відображається.
+// При кліку на кнопку "Відкрити" з'являється модальне вікно,
+// на кнопку "Закрити" — зникає.
+
+document.addEventListener('DOMContentLoaded', function() {
+    /* Записываем в переменные массив элементов-кнопок и подложку.
+     Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+    let modalButtons = document.querySelectorAll('.js-open-modal'),
+        overlay = document.querySelector('.js-overlay-modal'),
+        closeButtons = document.querySelectorAll('.js-modal-close');
+    
+    /* Перебираем массив кнопок */
+    modalButtons.forEach(function(item){
+        /* Назначаем каждой кнопке (если их больше одной )обработчик клика */
+        item.addEventListener('click', function(e) {
+            /* Предотвращаем стандартное действие элемента. Так как кнопку можно
+            сделать по-разному (ссылкой или кнопкой)*/
+            e.preventDefault();
+            /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+             и будем искать модальное окно с таким же атрибутом.*/
+            let modalId = this.getAttribute('data-modal'),
+                modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+            /* После того как нашли нужное модальное окно, добавим классы
+             подложке и окну, чтобы показать их. */
+            modalElem.classList.add('active');
+            overlay.classList.add('active');
+        });
+    });
+    
+    closeButtons.forEach(function(item){
+        item.addEventListener('click', function(e) {
+            let parentModal = this.closest('.modal');
+            parentModal.classList.remove('active');
+            overlay.classList.remove('active');
+        });
+    });
+    
+    document.body.addEventListener('keyup', function (e) {
+        let key = e.keyCode;
+        if (key === 27) {
+            document.querySelector('.modal.active').classList.remove('active');
+            document.querySelector('.overlay').classList.remove('active');
+        }
+    }, false);
+    overlay.addEventListener('click', function() {
+        document.querySelector('.modal.active').classList.remove('active');
+        this.classList.remove('active');
+    });
+});
