bf27a57f94f84795dbb38b407ccd92067cde0390
Dima Tatiana <Cheize2020@gmail.com>
2022-05-31 17:52:06 +0300

    changes 1

--- Lesson34DOM/assets/js/main.js
+++ Lesson34DOM/assets/js/main.js
@@ -1,1 +1,31 @@
 "use strict"
+
+
+
+
+
+//2. Створити HTML-сторінку з великою таблицею.
+// При кліку на заголовок стовпця, необхідно відсортувати дані
+// цього стовпця. Врахуй, що числові значення повинні сортуватися
+// як числа, а не як рядки.
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const getSort = ({ target }) => {
+        const order = (target.dataset.order = -(target.dataset.order || -1));
+        const index = [...target.parentNode.cells].indexOf(target);
+        const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
+        const comparator = (index, order) => (a, b) => order * collator.compare(
+            a.children[index].innerHTML,
+            b.children[index].innerHTML
+        );
+        
+        for(const tBody of target.closest('table').tBodies)
+            tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+        
+        for(const cell of target.parentNode.cells)
+            cell.classList.toggle('sorted', cell === target);
+    };
+    
+    document.querySelectorAll('.table_sort thead').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
+    
+});
