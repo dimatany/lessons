3c93ec9f69b122016098e2564cff212406df31ea
Dima Tatiana <Cheize2020@gmail.com>
2022-06-08 14:59:22 +0300

    changes 1

--- Lesson38CART/assets/js/main.js
+++ Lesson38CART/assets/js/main.js
@@ -1,101 +1,42 @@
 "use strict"
-/* - XMLHttpRequest() js native- устаревший вариант который никто не использует
-const xhr = new XMLHttpRequest();
-xhr.open('get','assets/data/cart.json');
-xhr.send();
-xhr.onreadystatechange = function() {
-    if(xhr.readyState===4){
-        if(xhr.status===200) {
-        console.log(xhr.response);
-        } else {
-            alert(xhr.statusText);
-        }
-    }
-}
-*/
-
-/* - fetch js native - код на чистом js
-fetch('assets/data/cart.json')
-    .then(resp=>{
-       return resp.json();
-})
-    .then(resp=>{
-    console.log(resp);
-    viewCart(resp);
-})
- */
-
-/*- ниже отрабатывает код с библиотеки axios
-axios('assets/data/cart.json')
-    .then(resp => {
-    viewCart(resp.data);
-})
-.cache(err=>{
-    alert(err.message)
-});
-*/
-
-/*
-//jQeary library вариант
-$.ajax({
-    url:'assets/data/cart.json',
-    type: 'get',
-    dataType:'json',
-    success: function(resp) {
-        console.log(resp);
-        viewCart(resp);
-    },
-    error: function(err) {
-        console.log(err);
-        alert(err.statusText);
-    }
-});//старый вариант кода
-
-$.ajax('assets/data/cart.json')
-    .done((resp)=>{
-            viewCart(resp);
-    })
-    .fail((err)=> {
-        alert(err.statusText);
-    });//новый вариант кода
-*/
-//короткий вариант запроса на jQeary
-// $.get('assets/data/cart.json', (resp)=>{viewCart(resp);});
-/*
-function viewCart(cart) {
-    let html = '<ul>';
-    cart.forEach(item=>{
-        html +=`
-        <li>
-        <b> ${item.name} </b> ${item.qty} - ${item.price}
-        </li>
-`;
+//карта гугл
+//50.387273, 30.624084
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 50.387273, lng: 30.624084 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
     });
-    html += '<ul>';
-    document.body.insertAdjacentHTML('afterBegin', html)
-}
-*/
-
-
-//Функция которая загружает страницу
-
-function loadPage(page) {
-    $.get('pages/' + page +'.html', (html)=>{
-        $('#page_content').html(html);
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
     });
 }
-loadPage('main');
-
-$(function() {
-    $('.nav-masthead a').on('click', function(e){
-        e.preventDefault();
-        loadPage($(this).attr('href'));
-        $('.nav-link. active').removeClass('active ');
-        $(this).addClass('active');
-    })
-});
+window.initMap = initMap;
 
 
+//карта leaflet
+let map2 = L.map('map2').setView([50.387273, 30.624084], 13);//координаты маркера
+
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map2);
+
+L.marker([50.387273, 30.624084]).addTo(map2)//координаты popup
+    .bindPopup('This is my place of residence.<br> Not for spread.')
+    .openPopup();
+
+L.marker([50.401849382118435, 30.632636297957657]).addTo(map2)//координаты popup
+    .bindPopup('Here they sell the most delicious fish in the city.<br> I\'m often there.')
+    .openPopup();
+
+L.marker([50.346753332881136, 30.48959863146808]).addTo(map2)//координаты popup
+    .bindPopup('This is my favorite park in town.<br> I\'m often there.')
+    .openPopup();
 
 
 
