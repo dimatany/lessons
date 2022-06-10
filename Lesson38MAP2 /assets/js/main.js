f8da601a165eca94969f539dd3abeaa140423cb8
Dima Tatiana <Cheize2020@gmail.com>
2022-06-10 14:43:43 +0300

    changes 1

--- Lesson38MAP2 /assets/js/main.js
+++ Lesson38MAP2 /assets/js/main.js
@@ -1,3 +1,4 @@
+'use strict'
 let tiles = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png\'', {
         maxZoom: 18,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
@@ -8,22 +9,8 @@ let map = L.map('map', {center: latlng, zoom: 13, layers: [tiles]});
 
 let markers = L.markerClusterGroup();
 
-for (let i = 0; i < addressPoints.length; i++) {
-    let a = addressPoints[i];
-    let title = a[2];
-    let marker = L.marker(L.latLng(a[0], a[1]), { title: title });
-    /*
-    if(a[3]===0) {
-        marker[{icon}]=myIcon;
-    }else if(a[3]===1) {
-        marker[{icon}]=myIcon2;
-    }else if(a[3]===2) {
-        marker[{icon}]=myIcon2;
-    }else {
-        return false;
-    }*/
-    marker.bindPopup(title);
+addressPoints.forEach(el=>{
+    let marker = L.marker(L.latLng(el.lat, el.lng), { title: el.descr, icon: Icons[el.type] }).bindPopup(el.descr);
     markers.addLayer(marker);
-}
-
+})
 map.addLayer(markers);
