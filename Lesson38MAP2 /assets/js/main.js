9d4bfc00d411b9c2026c54796136ee895e2feafe
Dima Tatiana <Cheize2020@gmail.com>
2022-06-12 12:00:09 +0300

    changes 1

--- Lesson38MAP2 /assets/js/main.js
+++ Lesson38MAP2 /assets/js/main.js
@@ -1,4 +1,50 @@
 'use strict'
+////////////////////////////////////////////////////////////////////////////////////////////////////
+const Icons = {
+    1:L.icon({
+        iconUrl: 'assets/img/marker.png',
+        iconSize: [48, 48],
+        iconAnchor: [22, 60],
+        popupAnchor: [-3, -76],
+    }),
+    2: L.icon({
+        iconUrl: 'assets/img/marker2.png',
+        iconSize: [48, 48],
+        iconAnchor: [22, 60],
+        popupAnchor: [-3, -76],
+    }),
+    3: L.icon({
+        iconUrl: 'assets/img/marker3.png',
+        iconSize: [48, 48],
+        iconAnchor: [22, 60],
+        popupAnchor: [-3, -76],
+    })
+}
+////////////////////////////////////////////////////////////////////////////////////////////////
+const addressPoints = [
+    {
+        lat: 50.387273,
+        lng: 30.624084,
+        img: 'assets/img/abstract.jpg',
+        descr: 'This is my place of residence.<br> Not for spread.',
+        type: 1,
+    },
+    {
+        lat: 50.401849382118435,
+        lng: 30.632636297957657,
+        img: 'assets/img/blue.jpg',
+        descr: 'Here they sell the most delicious fish in the city.<br> I\'m often there.',
+        type: 2,
+    },
+    {
+        lat: 50.346753332881136,
+        lng: 30.48959863146808,
+        img: 'assets/img/green.jpg',
+        descr: 'Here they sell the most delicious fish in the city.<br> I\'m often there.',
+        type: 3,
+    }
+];
+/////////////////////////////////////////////////////////////////////////////////////////////
 function initMap() {
 let tiles = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png\'', {
         maxZoom: 18,
@@ -7,50 +53,6 @@ let tiles = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/ligh
     latlng = L.latLng(50.387273, 30.624084);
 let map = L.map('map', {center: latlng, zoom: 13, layers: [tiles]});
     
-    const Icons = {
-        1:L.icon({
-            iconUrl: 'assets/img/marker.png',
-            iconSize: [48, 48],
-            iconAnchor: [22, 60],
-            popupAnchor: [-3, -76],
-        }),
-        2: L.icon({
-            iconUrl: 'assets/img/marker2.png',
-            iconSize: [48, 48],
-            iconAnchor: [22, 60],
-            popupAnchor: [-3, -76],
-        }),
-        3: L.icon({
-            iconUrl: 'assets/img/marker3.png',
-            iconSize: [48, 48],
-            iconAnchor: [22, 60],
-            popupAnchor: [-3, -76],
-        })
-    }
-    
-    let addressPoints = [
-        {
-            lat: 50.387273,
-            lng: 30.624084,
-            img: 'assets/img/abstract.jpg',
-            descr: 'This is my place of residence.<br> Not for spread.',
-            type: 1,
-        },
-        {
-            lat: 50.401849382118435,
-            lng: 30.632636297957657,
-            img: 'assets/img/blue.jpg',
-            descr: 'Here they sell the most delicious fish in the city.<br> I\'m often there.',
-            type: 2,
-        },
-        {
-            lat: 50.346753332881136,
-            lng: 30.48959863146808,
-            img: 'assets/img/green.jpg',
-            descr: 'Here they sell the most delicious fish in the city.<br> I\'m often there.',
-            type: 3,
-        }
-    ];
     
     let markers = L.markerClusterGroup();
     addressPoints.forEach(el=>{
