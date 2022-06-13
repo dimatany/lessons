c149e4dc264a496770d047c623a76a5a1740500c
Dima Tatiana <Cheize2020@gmail.com>
2022-06-13 16:02:48 +0300

    changes 1

--- Lesson40Exam/assets/js/main.js
+++ Lesson40Exam/assets/js/main.js
@@ -1,1 +1,15 @@
 "use strict"
+//header slider
+$(function() {
+    $('.slick_slider').slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+        fade: true,
+        pauseOnDotsHover: true,
+        
+    });
+});
+
+
+
