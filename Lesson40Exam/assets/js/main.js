ea564e4420f092acd2dafdc3795e190aa6acb8f8
Dima Tatiana <Cheize2020@gmail.com>
2022-06-17 16:58:56 +0300

    changes 1

--- Lesson40Exam/assets/js/main.js
+++ Lesson40Exam/assets/js/main.js
@@ -26,8 +26,8 @@ $('.wrap').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 2,
+                slidesToScroll: 2,
                 infinite: true,
                 dots: true
             }
@@ -35,17 +35,10 @@ $('.wrap').slick({
         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
             }
-        }
+        },
     ]
 });
 
