9e9622e84c2e3ae09c6a1f1c2aca68c4df9d310f
Dima Tatiana <Cheize2020@gmail.com>
2022-06-22 16:53:19 +0300

    changes 1

--- Lesson42Class/assets/js/main.js
+++ Lesson42Class/assets/js/main.js
@@ -1,57 +1,62 @@
 'use strict'
 ////////////////////////////////////////////////////////////////////////////////
 //1я задача
-document.querySelector(".firstCalc").addEventListener("click", function () {
-    const r = document.querySelector("#first").value;
-    const myCircle = new DescriptionCircle(r);
-    document.querySelector("#result").innerHTML += `
-        <div>Радиус круга: ${myCircle.radius}</div>
-        <div>Диаметр круга: ${myCircle.diametr}</div>
+{
+    document.querySelector(".firstCalc").addEventListener("click", function() {
+        const r = document.querySelector("#first").value;
+        const circle = new CircleDescription(r);
+        document.querySelector("#result").innerHTML += `
+        <div>Радиус круга: ${circle.radius}</div>
+        <div>Диаметр круга: ${circle.diametr}</div>
     `
-});
-
-document.querySelector(".secondCalc").addEventListener("click", function () {
-    const r = document.querySelector("#first").value;
-    const myCircle = new DescriptionCircle(r);
-    document.querySelector("#result").innerHTML += `
-        <div>Площадь круга: ${myCircle.circleSquare()}</div>
+    });
+    
+    document.querySelector(".secondCalc").addEventListener("click", function() {
+        const r = document.querySelector("#first").value;
+        const circle = new CircleDescription(r);
+        document.querySelector("#result").innerHTML += `
+        <div>Площадь круга: ${circle.circleSquare()}</div>
     `
-});
-
-document.querySelector(".thirdCalc").addEventListener("click", function () {
-    const r = document.querySelector("#first").value;
-    const myCircle = new DescriptionCircle(r);
-    document.querySelector("#result").innerHTML += `
+    });
+    
+    document.querySelector(".thirdCalc").addEventListener("click", function() {
+        const r = document.querySelector("#first").value;
+        const myCircle = new CircleDescription(r);
+        document.querySelector("#result").innerHTML += `
         <div>Длина круга: ${myCircle.circleLength()}</div>
     `
-});
+    });
+}
+////////////////////////////////////////////////////////////////////////////////
+//2я задача
+
 
 ////////////////////////////////////////////////////////////////////////////////
 //3я задача
-const list = [
-    new Employee({
-        name:'Bob',
-        position:'manager',
-        age:30,
-        qualification:'low',
-    }),
-    new Employee({
-        name:'John',
-        position:'High manager',
-        age:35,
-        qualification:'high',
-    }),
-    new Employee({
-        name:'Sara',
-        position:'manager',
-        age:27,
-        qualification:'average',
-    }),
-];
-
-const myTable = new EmpTable(list);
-myTable.viewHtml('emp_tbody');
-
-
+{
+    const list = [
+        new Employee({
+            name: 'Bob',
+            position: 'manager',
+            age: 30,
+            qualification: 'low',
+        }),
+        new Employee({
+            name: 'John',
+            position: 'High manager',
+            age: 35,
+            qualification: 'high',
+        }),
+        new Employee({
+            name: 'Sara',
+            position: 'manager',
+            age: 27,
+            qualification: 'average',
+        }),
+    ];
+    
+    const myTable = new EmpTable(list);
+    myTable.viewHtml('emp_tbody');
+}
 
 
