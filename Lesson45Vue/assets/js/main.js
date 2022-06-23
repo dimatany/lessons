85c232fa8d6f9636d036afe1c052fd9a0e3832eb
Dima Tatiana <Cheize2020@gmail.com>
2022-06-23 13:13:01 +0300

    changes 1

--- Lesson45Vue/assets/js/main.js
+++ Lesson45Vue/assets/js/main.js
@@ -1,1 +1,20 @@
 'use strict'
+const App = {
+    data(){
+        return {
+            todoList: [],
+            taskText: '',
+            
+        }
+    },
+    methods: {
+        addTodo() {
+            this.todoList.push({
+                text: this.taskText,
+                isDone: false,
+            });
+        }
+    }
+}
+
+Vue.createApp(App).mount('#app')
