3ed504bf334edfe9fec16e03e57ee04c366d64b0
Dima Tatiana <Cheize2020@gmail.com>
2022-06-23 17:06:14 +0300

    changes 1

--- Lesson45Vue/assets/js/main.js
+++ Lesson45Vue/assets/js/main.js
@@ -1,18 +1,51 @@
 'use strict'
+
+const TodoItem = {
+    name: 'TodoItem',
+    props:['item', 'index'],
+    methods: {
+        markDone(ind) {
+            this.$parent.markDone(ind);
+        }
+    },
+    template: '#todo_item'
+}
 const App = {
     data(){
         return {
-            todoList: [],
+            todoList: [
+                {
+                    text: 'By milk',
+                    isDone: true,
+                },
+                {
+                    text: 'Learn Vue.JS',
+                    isDone: false,
+                },
+                {
+                    text: 'Wash car',
+                    isDone: false,
+                }
+            ],
             taskText: '',
             
         }
     },
+    components: {
+        TodoItem
+    },
     methods: {
         addTodo() {
-            this.todoList.push({
-                text: this.taskText,
-                isDone: false,
-            });
+            if(this.taskText !=='') {
+                this.todoList.push({
+                    text: this.taskText,
+                    isDone: false,
+                });
+                this.taskText = '';
+            }
+        },
+        markDone(ind){
+            this.todoList[ind].isDone = true;
         }
     }
 }
